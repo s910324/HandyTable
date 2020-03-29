e7c2500e7d3c8b1dc1b530b91c0ec89ab02d1aab
s910324 <scott910324@gmail.com>
2020-03-29 23:41:29 +0800

    update spec setting, crosshair visibility, barchart spacing and width

--- FreezeView/Graph/Distribution.py
+++ FreezeView/Graph/Distribution.py
@@ -24,26 +24,75 @@ class DistributionWidget(QWidget):
     valueSelected = pyqtSignal(object)
     def __init__(self,  parent=None):
         super(DistributionWidget, self).__init__(parent)
-        plot_widget1 = DistributionChartWidget()
-        plot_widget2 = DistributionChartWidget()
-        self.setLayout(HBox(plot_widget1, plot_widget2))
+        self._plot_widget1 = DistributionChartWidget()
+        self._plot_widget2 = DistributionChartWidget()
+        self._spec_h       = QLineEdit()
+        self._spec_tgt     = QLineEdit()
+        self._spec_l       = QLineEdit()
+        self._pb1          = QPushButton("set")
+
+        self._bar_width    = QLineEdit()
+        self._pb2          = QPushButton("set")
+
+        self._bar_offset   = QLineEdit()
+        self._pb3          = QPushButton("set")
+
+        self._pb4          = QPushButton("set")
+
+        self.setLayout(
+            VBox(
+                HBox(self._plot_widget1),#, self._plot_widget2),
+                HBox(self._spec_h, self._spec_tgt, self._spec_l, self._pb1),
+                HBox(self._bar_width,  self._pb2),
+                HBox(self._bar_offset, self._pb3),
+                HBox(self._pb4)
+                )
+            )
+        self._pb1.clicked.connect(self.setSpec)
+        self._pb2.clicked.connect(self.setBarWidth)
+        self._pb3.clicked.connect(self.setBarSpacing)
+        self._pb4.clicked.connect(self.setCrosshair)
 
 
         csv = pd.read_csv(r'C:\Users\rawr\Downloads\MOCK_DATA (4).csv')
         s1 = Statistic(data = csv.iloc[:, 3], spec_high=30, spec_low=20)
         s2 = Statistic(data = csv.iloc[:, 2], spec_high=30, spec_low=20)
-        plot_widget1.AddData(self.g(s1))
-        plot_widget1.AddData(self.g(s2))
+        s3 = Statistic(data = csv.iloc[:, 1], spec_high=30, spec_low=20)
+        s4 = Statistic(data = csv.iloc[:, 0], spec_high=30, spec_low=20)
+        self._plot_widget1.AddData(self.g(s1))
+        self._plot_widget1.AddData(self.g(s2))
+        self._plot_widget1.AddData(self.g(s3))
+        self._plot_widget1.AddData(self.g(s4))
 
     def g(self, data):
         d    = (data.frequency_chart(scale = [15, 40, 1]))
         x, y = [ ix for ix, iy in d], [ iy for ix, iy in d]
         return x, y
 
+    def setSpec(self):
+        h = float(self._spec_h.text())
+        t = float(self._spec_tgt.text())
+        l = float(self._spec_l.text())
+        self._plot_widget1.Spec = [l, t, h]
+
+    def setBarWidth(self):
+        w = float(self._bar_width.text())
+        self._plot_widget1.BarWidth = w
+
+    def setBarSpacing(self):
+        s = float(self._bar_offset.text())
+        self._plot_widget1.BarOffset = s
+    
+    def setCrosshair(self):
+        self._plot_widget1.CrosshairVisible(not(self._plot_widget1._crosshair_visible))
+
 class DistributionChartWidget(pg.PlotWidget):
     def __init__(self,  parent=None):
         super(DistributionChartWidget, self).__init__(parent)
+        self._bar_width         = 1
+        self._bar_offset        = 0
         self._charts            = []
+        self._chart_data        = []
         self._auto_scale        = True
         self._spec_h            = None
         self._target            = None
@@ -58,14 +107,13 @@ class DistributionChartWidget(pg.PlotWidget):
         self._spec_h_line       = pg.InfiniteLine(angle = 90, movable = False)
         self._target_line       = pg.InfiniteLine(angle = 90, movable = False)
         self._spec_l_line       = pg.InfiniteLine(angle = 90, movable = False)     
-
+        self._color_theme       = [(255, 6, 120, 100), (255, 172, 0, 100), (0, 189, 255, 100), (0, 80, 255, 100), (189, 0, 255, 100), (130, 130, 130, 100)]
         self._init_()
         self.CrosshairVisible()
     
     def _init_(self):
-        self.ViewBox.setBackgroundColor(QColor("#ffffff"))
-        self.setBackground(QColor("#dddddd"))
-        
+        self.ViewBoxColor    = "#ffffff"
+        self.BackgroundColor = "#dddddd"
         default_label_style  = {'font-size':'12', 'font-family':'Arial', 'color':'#656565'}
         default_tick_style   = {'showValues':  True, 'tickLength': -3, 'tickTextOffset':  5, 'tickFont':'Arial'}
         hidden_tick_style    = {'showValues': False, 'tickLength':  0, 'tickTextOffset':  5, 'tickFont':'Arial'}
@@ -80,7 +128,7 @@ class DistributionChartWidget(pg.PlotWidget):
 
     def AddData(self, data):
         x, y  = data
-        chart = pg.BarGraphItem(x=x, height=y, width=0.5, brush=(253,6,200, 100))
+        chart = pg.BarGraphItem(x=np.array(x), height=y, width=self._bar_width, brush = self._color_theme[len(self)], x_offset = 0)
         self.addItem(chart)
         self._charts.append(chart)
         self.AxisRange = [None, None]
@@ -90,6 +138,35 @@ class DistributionChartWidget(pg.PlotWidget):
         self.TargetLineVisible = True
         self.SpecLLineVisible  = True
         return chart
+
+    
+    def __len__(self):
+        return len(self._charts)
+    
+
+    @property
+    def BarWidth(self):
+        return self._bar_width
+
+    @BarWidth.setter
+    def BarWidth(self, width):
+        if  (0 < float(width) <= 1):
+            self._bar_width = width
+            for chart in self._charts:
+                chart.setOpts(width= width)
+
+    @property
+    def BarOffset(self):
+        return self._bar_offset
+
+    @BarOffset.setter
+    def BarOffset(self, offset):
+        if  (0 < float(offset) <= 1):
+            self._bar_offset = offset
+            for index, chart in enumerate(self._charts):
+                x, old_offset, new_offset = chart.opts['x'], chart.opts['x_offset'], (self._bar_offset * index)
+                delta                     = new_offset - old_offset
+                chart.setOpts(x = np.array(x) + delta, x_offset = new_offset)
 
     def CrosshairVisible(self, visible = True):
         self._crosshair_visible = visible
@@ -101,10 +178,13 @@ class DistributionChartWidget(pg.PlotWidget):
             for line in [self._crosshair_hline, self._crosshair_vline]:
                 line.setPen(pg.mkPen(color = pg.mkColor("#aaaaaa"), style = Qt.DotLine))
                 self.PlotItem.addItem(line, ignoreBounds=True)
+        else:
+            for item in [self._crosshair_label, self._crosshair_hline, self._crosshair_vline]:
+                self.PlotItem.removeItem(item)
         
 
     def mouseMoved(self, pos):
-        if self.ViewBox.sceneBoundingRect().contains(pos):
+        if self.ViewBox.sceneBoundingRect().contains(pos) and self._crosshair_visible:
             point = self.ViewBox.mapSceneToView(pos)
             self._crosshair_label.setText("(%0.1f, %0.1f)" % (point.x(), point.y()))
             self._crosshair_label.setPos(QPointF(point.x(), point.y()))
@@ -122,6 +202,9 @@ class DistributionChartWidget(pg.PlotWidget):
     def Spec(self, spec_list):
         if (len(spec_list) == 3 and all([type(item) in [int, float, type(None)] for item in spec_list])):
             self._spec_l, self._target, self._spec_h = spec_list
+            self.SpecHLineVisible  = self._spec_h_line_show
+            self.TargetLineVisible = self._target_line_show
+            self.SpecLLineVisible  = self._spec_l_line_show
         else:
             raise TypeError("item %s with object type %s is not supported) " % (spec_list, type(spec_list))) 
 
@@ -246,6 +329,21 @@ class DistributionChartWidget(pg.PlotWidget):
             raise TypeError("item %s with object type %s is not supported) " % (y_range, type(y_range)))
     
 
+    @property
+    def ViewBoxColor(self):
+        return self.ViewBox.state['background'].name()
+
+    @ViewBoxColor.setter
+    def ViewBoxColor(self, color):
+        self.ViewBox.setBackgroundColor(QColor(color))
+ 
+    @property
+    def BackgroundColor(self):
+        return self.backgroundBrush().color().name()
+
+    @BackgroundColor.setter
+    def BackgroundColor(self, color):
+        self.setBackground(QColor(color))
 
 
 if __name__ == "__main__":
